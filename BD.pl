10111d251e56bc0b8b825dd9f962e091394e054a
Victor Padilla <v.padilla.1@estudiantec.cr>
2026-04-27 01:31:31 -0600

    implementación de base de datos de las carreras

--- BD.pl
+++ BD.pl
@@ -1,4 +1,6 @@
 %Inicio de la base de datos de profesiones
+
+% profesion(nombre, [afinidades], [habilidades], [antagonias])
 profesion(ingenieria_computadores,
     [matematicas, tecnologia, resolver_problemas],
     [logica],
@@ -8,3 +10,43 @@ profesion(psicologia,
     [personas, escuchar, ayudar],
     [empatia],
     [matematicas]).
+
+profesion(medicina,
+    [biologia, ayudar, personas],
+    [memoria, dedicacion],
+    [tecnologia, arte]).
+
+profesion(diseno_grafico,
+    [arte, creatividad, tecnologia],
+    [atencion_al_detalle],
+    [matematicas, biologia]).
+
+profesion(derecho,
+    [leer, argumentar, personas],
+    [memoria, logica],
+    [tecnologia, biologia]).
+
+profesion(administracion_empresas,
+    [liderazgo, personas, matematicas],
+    [organizacion, comunicacion],
+    [arte, biologia]).
+
+profesion(ingenieria_civil,
+    [matematicas, construir, resolver_problemas],
+    [logica, atencion_al_detalle],
+    [arte, escuchar]).
+
+profesion(educacion,
+    [personas, ayudar, comunicar],
+    [paciencia, empatia],
+    [tecnologia, matematicas]).
+
+profesion(biologia,
+    [biologia, naturaleza, investigar],
+    [paciencia, atencion_al_detalle],
+    [tecnologia, liderazgo]).
+
+profesion(contabilidad,
+    [matematicas, organizacion, liderazgo],
+    [atencion_al_detalle, logica],
+    [arte, personas]).
